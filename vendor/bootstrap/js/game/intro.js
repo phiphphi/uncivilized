b7dbd0910c9e4e528d9f9a8c32b79e244a679bf0
phi <pton@uw.edu>
2020-07-29 16:01:33 -0700

    Working on game initial experience

--- vendor/bootstrap/js/game/intro.js
+++ vendor/bootstrap/js/game/intro.js
@@ -5,3 +5,4 @@ function introInit() {
     $("#alert-container").html("<div class='alert alert-custom alert-dismissible fade show'>" + stats.introText +
         "<button type='button' class='close' data-dismiss='alert'>&times;</button></div>");
 }
+
