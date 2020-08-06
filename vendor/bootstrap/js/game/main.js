9fece495554ac31380948652ce4ec398aa376deb
phi <pton@uw.edu>
2020-08-05 19:39:59 -0700

    Added improvements to tour layout

--- vendor/bootstrap/js/game/main.js
+++ vendor/bootstrap/js/game/main.js
@@ -19,19 +19,21 @@ let introTour = new Tour({
         {
             element: "#city-card",
             title: "Welcome to uncivilized!",
-            content: "This quick tour will guide you through the basics of the game."
+            content: "This quick tour will guide you through the basics of the game.",
+            placement: "bottom"
         },
         {
             element: "#resources-card",
             title: "Resources, human and not",
-            content: "Your currently unlocked resources, their max capacity, and net production per second. Note that " +
-                "your workers are a bit different - the left count tracks how many you have available, and the right count " +
-                "tracks how much available room you have to recruit more."
+            content: "Your currently unlocked resources, capacity, & production. Note that your workers are a bit " +
+                "different - the right count tracks how much room you have to recruit more.",
+            placement: "left"
         },
         {
             element: "#city-card",
             title: "Your city's status",
-            content: "This keeps track of your total amount of workers and size overall."
+            content: "This keeps track of your total amount of workers and size overall.",
+            placement: "bottom"
         },
         {
             element: "#research-card",
@@ -41,15 +43,16 @@ let introTour = new Tour({
         {
             element: "#infrastructure-tab",
             title: "The foundation of society",
-            content: "Of note is the infrastructure tab, which is different than the others - instead of providing resource " +
-                "production, these buildings provide capacity for extra workers, which you can also hire under the workers tab."
+            content: "Of note is the infrastructure tab - instead of providing resource production, these buildings " +
+                "provide capacity for extra workers, which you can also hire under the workers section.",
+            placement: "top"
         },
         {
             element: "#city-card",
             title: "Wrap it up",
             content: "That's all you need for now! I recommend buying some woodcutters to start producing materials and some " +
-                "tents to hire new workers, as well as researching new technologies - you'll need to find a way to produce extra " +
-                "water, since that oasis can only support so many. Good luck!"
+                "tents to hire new workers, then looking into new technologies!",
+            placement: "bottom"
         },
     ],
     framework: 'bootstrap4',
